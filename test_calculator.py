bbcb4b92e36feaba520984198702cb55933a9bca
EmanElkalawy <emanelkalawy018@gmail.com>
2024-11-16 22:02:14 +0200

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -5,7 +5,7 @@ from calculator import Calculator
 class TestCalculator(unittest.TestCase):
     def test_add(self): 
       self.assertEqual(Calculator().add(2, 3), 5)
-      self.assertEqual(Calculator().add(-2,-3),-8)
+      self.assertEqual(Calculator().add(-2,-3),-5)
       self.assertEqual(Calculator().add(2, -3), -1)
 
 if __name__ == '__main__':
